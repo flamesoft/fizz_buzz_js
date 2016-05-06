d2bb95d48966bdbc86b322b86b2f77a1d9d7e64b
Yan Zhang <emma2740@gmail.com>
2016-05-06 15:06:58 +0200

    Add divisible by 5 test and method

--- assets/js/fizz_buzz.js
+++ assets/js/fizz_buzz.js
@@ -4,6 +4,9 @@ Fizz_buzz.prototype.fizz_buzz = function(number) {
   if(number%3 == 0){
     return 'fizz';
   }
+  else if(number%5 == 0){
+    return 'buzz';
+  }
   else{
     return number.toString();
   }
--- spec/fizz_buzz_spec.js
+++ spec/fizz_buzz_spec.js
@@ -12,4 +12,7 @@ describe("fizz_buzz", function () {
     it("returns 'fizz' if number is divisible by 3", function () {
         expect(subject.fizz_buzz(33)).toEqual('fizz');
     });
+    it("returns 'buzz' if number is divisible by 5", function () {
+        expect(subject.fizz_buzz(10)).toEqual('buzz');
+    });
 });
