0473954042c7a5ac4414d6e22a3ad33d5d492a44
Yan Zhang <emma2740@gmail.com>
2016-05-06 15:12:01 +0200

    Add divisible by 15 test and implementation

--- assets/js/fizz_buzz.js
+++ assets/js/fizz_buzz.js
@@ -1,7 +1,10 @@
 function Fizz_buzz() {};
 
 Fizz_buzz.prototype.fizz_buzz = function(number) {
-  if(number%3 == 0){
+  if(number%15 == 0){
+    return 'fizz buzz';
+  }
+  else if(number%3 == 0){
     return 'fizz';
   }
   else if(number%5 == 0){
@@ -10,4 +13,5 @@ Fizz_buzz.prototype.fizz_buzz = function(number) {
   else{
     return number.toString();
   }
+
 }
--- spec/fizz_buzz_spec.js
+++ spec/fizz_buzz_spec.js
@@ -8,11 +8,14 @@ describe("fizz_buzz", function () {
     afterEach(function () {
         /* ... Tear it down ... */
     });
-
+    it("returns 'fizz buzz' if number is divisible by 15", function (){
+        expect(subject.fizz_buzz(15)).toEqual('fizz buzz');
+    });
     it("returns 'fizz' if number is divisible by 3", function () {
         expect(subject.fizz_buzz(33)).toEqual('fizz');
     });
     it("returns 'buzz' if number is divisible by 5", function () {
         expect(subject.fizz_buzz(10)).toEqual('buzz');
     });
+
 });
